96914c4f60a9a971c90b06a88c4fbcf8e2ad4cc3
vithorDeveloper <vitorsantosdev663@gmail.com>
2024-01-15 21:18:03 -0300

    construção de hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 function styles() {
   return gulp.src("./src/styles/*.scss")
@@ -7,7 +8,13 @@ function styles() {
     .pipe(gulp.dest("./build/styles"))
 }
 
-exports.default = styles;
+function images() {
+  return gulp.src("./src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("./build/images"))
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
-  gulp.watch("./src/styles/*.scss", gulp.parallel(styles))
+  gulp.watch("./src/styles/layouts/_hero.scss", gulp.parallel(styles))
 }
